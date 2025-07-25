1ee4157fd3ef51cb94f3001326850f2f5546b021
Galí Estudio <galiestudiodev@gmail.com>
2025-07-25 18:34:49 +0000

    modifica el número de whatsapp por este 5493825405976

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const WHATSAPP_NUMBER = "5491122334455"; // Replace with actual Go aventura WhatsApp number
+export const WHATSAPP_NUMBER = "5493825405976"; // Replace with actual Go aventura WhatsApp number
 export const WHATSAPP_API_BASE_URL = "https://wa.me/";
