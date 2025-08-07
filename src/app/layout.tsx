038f2c5b148c0a920c5ee807a1b46f7f0ab4ea3e
Galí Estudio <galiestudiodev@gmail.com>
2025-08-07 13:45:10 +0000

    mira este diagnóstico realizado por PageSpeed Insights https://pagespeed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -89,10 +89,6 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700;800;900&family=Roboto:ital,wght@0,400;0,500;0,700;1,400&family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet" />
-        
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Roboto', 'sans-serif'],
-        headline: ['Montserrat', 'sans-serif'],
-        code: ['Source Code Pro', 'monospace'],
+        body: ['var(--font-roboto)', 'sans-serif'],
+        headline: ['var(--font-montserrat)', 'sans-serif'],
+        code: ['var(--font-source-code-pro)', 'monospace'],
       },
       colors: {
         background: 'hsl(var(--background))',
